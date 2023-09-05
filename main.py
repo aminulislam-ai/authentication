4069fe97ba4a1fd9b159a4327762ad1842da5a11
Aminul Islam <aminulislam@Aminuls-MacBook-Pro-2.local>
2023-09-05 06:25:10 +0600

    prepare base model completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Authentication import logger
 from Authentication.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Authentication.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -8,6 +9,19 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+
+try:
+    logger.info(f"****************************************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
